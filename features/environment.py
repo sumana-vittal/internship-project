cc5d7597765c101286fe3ac5d579fe3aee2ac9cd
sumana-sdet <v.sumana@gmail.com>
2024-03-21 17:46:53 -0700

    CAP-308 : modify code for the cloud testing - Browserstack

--- features/environment.py
+++ features/environment.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
@@ -9,29 +10,45 @@ from app.application import Application
 browser = "headless"
 
 
-def browser_init(context):
+def browser_init(context, scenario_name):
     """
     :param context: Behave context
     """
-    if browser.lower() == "chrome":
-        driver_path = ChromeDriverManager().install()
-        service = Service(driver_path)
-        context.driver = webdriver.Chrome(service=service)
-    elif browser.lower() == "firefox":
-        driver_path = GeckoDriverManager().install()
-        service = Service(driver_path)
-        context.driver = webdriver.Firefox(service=service)
-    elif browser.lower() == "headless":
-        options = webdriver.ChromeOptions()
-        options.add_argument("headless")
-        options.add_argument("window-size=1920,1080")
-        service = Service(ChromeDriverManager().install())
-        context.driver = webdriver.Chrome(
-            options=options,
-            service=service
-        )
-    else:
-        print("Please provide the valid browser name")
+    # if browser.lower() == "chrome":
+    #     driver_path = ChromeDriverManager().install()
+    #     service = Service(driver_path)
+    #     context.driver = webdriver.Chrome(service=service)
+    # elif browser.lower() == "firefox":
+    #     driver_path = GeckoDriverManager().install()
+    #     service = Service(driver_path)
+    #     context.driver = webdriver.Firefox(service=service)
+    # elif browser.lower() == "headless":
+    #     options = webdriver.ChromeOptions()
+    #     options.add_argument("headless")
+    #     options.add_argument("window-size=1920,1080")
+    #     service = Service(ChromeDriverManager().install())
+    #     context.driver = webdriver.Chrome(
+    #         options=options,
+    #         service=service
+    #     )
+    # else:
+    #     print("Please provide the valid browser name")
+
+    ###BrOWSERSTACK -runing testcases on cloud###
+    bs_user = "mashka_t1wvgX"
+    bs_key = "zDWkbUj7bhMyPsXx1Ds2"
+    url = f"https://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub"
+
+    options = Options()
+    bstack_options = {
+        'os': 'Windows',
+        'osVersion': '11',
+        'browserName': 'Firefox',
+        'sessionName': scenario_name
+    }
+
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
@@ -39,18 +56,9 @@ def browser_init(context):
     context.app = Application(context.driver)
 
 
-# def pytest_adoption(parser):
-#     parser.adoption("--browser")
-#
-#
-# @pytest.fixture(scope="class", autouse=True)
-# def browser(request):
-#     return request.config.getoption("--browser")
-
-
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    browser_init(context, scenario.name)
 
 
 def before_step(context, step):
